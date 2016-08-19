b00fd11df7d7ca88c9dc460c73c3ca713f597d76
Ivan Itzcovich <i.itzcovich@gmail.com>
2016-08-19 20:52:42 -0300

    Add zoom option

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import askopenfilename, asksaveasfilename
 from PIL import ImageTk, Image
 
 import sys
@@ -23,38 +23,47 @@ class GUI(tk.Frame):
     def create_subframes(self):
         
         self.original = tk.Frame(self)
-        self.working = tk.Frame(self)
+        self.studio = tk.Frame(self)
         self.menu = tk.Frame(self, bd=1)
 
         self.original.pack(fill=tk.BOTH, expand=tk.YES, side=tk.LEFT)
-        self.working.pack(fill=tk.BOTH, expand=tk.YES, side=tk.LEFT)
+        self.studio.pack(fill=tk.BOTH, expand=tk.YES, side=tk.LEFT)
         self.menu.pack(fill=tk.Y, expand=tk.YES,  side=tk.RIGHT)
 
         self.menu.label = tk.Label(self.menu, text="Menu")
-        self.menu.label.grid(pady=10, sticky=tk.W+tk.E+tk.N+tk.S)
+        self.menu.label.grid(pady=10)
         self.menu.button = tk.Button(self.menu, text="Load Image", command=self.load_file, width=10)
         self.menu.button.grid(pady=10)
         self.menu.button2 = tk.Button(self.menu, text="Revert", command=self.revert, width=10)
         self.menu.button2.grid(pady=10)
+        self.menu.button3 = tk.Button(self.menu, text="Save", command=self.save_file, width=10)
+        self.menu.button3.grid(pady=10)
+
+        self.menu.zoom = tk.Canvas(self.menu, height=150, width=150)
+        self.menu.zoom.grid()
+        self.menu.zoom.create_text(10,10, anchor='nw',text="Click Image to zoom")
+
+        self.menu.slider = tk.Scale(self.menu, from_=0, to=255, orient=tk.HORIZONTAL)
+        self.menu.slider.grid(pady=10)
 
         self.original.label = tk.Label(self.original, text="Original")
-        self.working.label = tk.Label(self.working, text="Working Copy")
+        self.studio.label = tk.Label(self.studio, text="Studio")
 
         self.original.canvas = tk.Canvas(self.original)
-        self.working.canvas = tk.Canvas(self.working)
+        self.studio.canvas = tk.Canvas(self.studio)
 
         self.original.label.pack(side=tk.TOP)
-        self.working.label.pack()
+        self.studio.label.pack()
 
         self.original.canvas.pack(fill=tk.BOTH, expand=tk.YES)
-        self.working.canvas.pack(fill=tk.BOTH, expand=tk.YES)
-
+        self.studio.canvas.pack(fill=tk.BOTH, expand=tk.YES)
+        self.studio.canvas.bind("<B1-Motion>", self.zoom)
+        self.studio.canvas.bind("<Button-1>", self.zoom)
 
     def revert(self):
         self.img = self.img.rotate(180)
         self.imagerotated = ImageTk.PhotoImage(self.img)
-        # self.working.canvas.config(image=self.image)
-        self.working.canvas.itemconfig(self.id, image = self.imagerotated)
+        self.studio.canvas.itemconfig(self.id, image = self.imagerotated)
 
     def load_file(self):
         fname = askopenfilename()
@@ -62,8 +71,18 @@ class GUI(tk.Frame):
             self.img = Image.open(fname)
             imgtk = ImageTk.PhotoImage(self.img)
             self.image = imgtk
-            self.id = self.working.canvas.create_image(0, 0, anchor='nw',image=self.image)
+            self.id = self.studio.canvas.create_image(0, 0, anchor='nw',image=self.image)
             self.original.canvas.create_image(0, 0, anchor='nw',image=self.image)
+
+    def save_file(self):
+        fname = asksaveasfilename()
+        if fname:
+            self.img.save(fname)
+
+    def zoom(self, event):
+        self.imgCropped = self.img.crop((event.x-20, event.y-20, event.x+20, event.y+20)).resize((150,150))
+        self.imagetk = ImageTk.PhotoImage(self.imgCropped)
+        self.menu.zoom.create_image(0, 0, anchor='nw',image=self.imagetk)
 
 
 if __name__ == "__main__":
